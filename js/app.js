8f7ec2643f28906b160af7fbf662c01ba82aafca
Allef <allefsousaicm1994@gmail.com>
2020-04-22 11:12:56 -0300

    Atualizações

--- js/app.js
+++ js/app.js
@@ -3,14 +3,25 @@ var app = new Vue({
   data: {
     tarefas: [],
 
-    novaTarefa: { title: "", estado: false },
+    novaTarefa: {
+      title: "",
+      estado: false
+    },
   },
   methods: {
     addTarefa: function () {
       if (this.novaTarefa.title.length > 0) {
-        this.tarefas.push(this.novaTarefa);
-        this.novaTarefa = "";
+        this.tarefas.push({
+          ...this.novaTarefa
+        });
+        this.novaTarefa.title = ""
       }
+    },
+    alterarStatus(tarefa) {
+      tarefa.estado = !tarefa.estado
+    },
+    removerTarefa(tarefa) {
+      this.tarefas.splice(this.tarefas.indexOf(tarefa), 1)
     },
   },
 });
